1f84079d4fa5f690ceb089f7bca0537b38b23f98
Nathan Hill <pjsstp@gmail.com>
2021-01-01 16:53:58 -0500

    updated to final version

--- src/App.js
+++ src/App.js
@@ -31,7 +31,13 @@ class App extends Component {
     this.setState({
       spellsThisTurn: 0
     })
+  }
 
+  resetApp = () => {
+    this.setState({
+      lifeTotal : 40,
+      spellsThisTurn : 0,
+    })
   }
 
   deathRay = () => {
@@ -48,87 +54,121 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* <div className="background-image">
+          <img src="./images/Aetherflux2-md.jpg" alt="Aetherflux Reservoir"
+              className="the-background"/>
+        </div> */}
+
         <header className="App-header">
-          Aetherflux Reservoir Counter
+          Aetherflux Reservoir
+          <br/>
+          Control Panel
         </header>
         <div className="container">
 
-          {/* HEADER ROW */}
           <div className="row">
-            <div className="col-6">
-              SPELLS CAST
+            <div className="col-1 spacer"></div>  
+            <div className="col-10 text-label-1">
+              Spells Cast = {this.state.spellsThisTurn}
             </div>
-            <div className="col-6">
-              LIFE TOTAL
-            </div>
-          </div>
-
-          {/* Data Row */}
-          <div className="row">
-            <div className="col-6">
-              {this.state.spellsThisTurn}
-            </div>
-            <div className="col-6">
-              {this.state.lifeTotal}
-            </div>
+            <div className="col-1 spacer"></div>  
           </div>
 
           <div className="row">
-            <div className="col-3">
-              <button className="btn btn-danger"
-                      onClick={() => this.updateLife(5, "lose")}>
+            <div className="col-1 spacer"></div>
+            <div className="col-10 text-label-2">
+              Life Total = {this.state.lifeTotal}
+            </div>
+            <div className="col-1 spacer"></div>  
+          </div>
+
+          <div className="row justify-content-center notation">
+            <div className="col-2"></div>
+            <div className="col-8">
+              Adjust Life Totals
+            </div>
+            <div className="col-2"></div>
+          </div>
+
+          <div className="row justify-content-center">
+            <div className="col-2 button-holder">
+              <div className="faux-baxton"
+                   onClick={() => this.updateLife(5, "lose")}>
+
                 -5
-              </button>
+              </div>
             </div>
-            <div className="col-3">
-              <button className="btn btn-danger"
-                      onClick={() => this.updateLife(1, "lose")}>
-
+            <div className="col-3 button-holder">
+              <div className="faux-baxton"
+                   onClick={() => this.updateLife(1, "lose")}>
                 -1
-              </button>
+              </div>
             </div>
-            <div className="col-3">
-              <button className="btn btn-danger"
-                      onClick={() => this.updateLife(1, "add")}>
-
+            <div className="col-3 button-holder">
+              <div className="faux-baxton"
+                   onClick={() => this.updateLife(1, "add")}>
                 +1
-              </button>
+              </div>
             </div>
-            <div className="col-3">
-              <button className="btn btn-danger"
-                      onClick={() => this.updateLife(5, "add")}>
-
+            <div className="col-2 button-holder">
+              <div className="faux-baxton" 
+                   onClick={() => this.updateLife(5, "add")}>
                 +5
-              </button>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-6">
-              <button className="btn btn-dark"
-                      onClick={() => this.addSpell()}>
-                CAST A SPELL
-              </button>
-            </div>
-            <div className="col-6">
-              <button className="btn btn-warning"
-                      onClick={() => this.deathRay()}>
-                FIRE DEATH RAY
-              </button>
+              </div>
             </div>
           </div>
 
           <div className="row">
             <div className="col">
-              <button className="btn btn-primary"
-                      onClick={() => this.resetSpellCount()}>
-                END TURN
-              </button>
+              <div className="vertical-spacer"></div>
+            </div>
+          </div>
+
+          <div className="row justify-content-center">
+            <div className="col-5 button-holder">
+              <div className="faux-baxton-lg"
+                      onClick={() => this.addSpell()}>
+                Cast a Spell
+              </div>
+            </div>
+            <div className="col-5 button-holder">
+            <div className="faux-baxton-lg"
+                      onClick={() => this.deathRay()}>
+                Fire Death Ray
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col">
+              <div className="vertical-spacer"></div>
             </div>
           </div>
 
 
+          <div className="row justify-content-center">
+          <div className="col-8 col-sm-6 button-holder">
+              <div className="faux-baxton"
+                      onClick={() => this.resetSpellCount()}>
+                End Turn
+              </div>
+            </div>
+          </div>
 
+          <div className="row">
+            <div className="col">
+              <div className="vertical-spacer"></div>
+            </div>
+          </div>
+
+          <div className="row justify-content-center">
+            <div className="col-8 col-sm-6 button-holder">
+              <div className="faux-baxton-thin"
+                      onClick={() => this.resetApp()}>
+                Reset App
+              </div>
+            </div>
+          </div>
 
         </div>
       </div>
